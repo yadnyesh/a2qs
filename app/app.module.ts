afd5843b1753017479845dfc0b3a35167a94009c
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2016-09-25 18:10:59 +0530

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+
+import {MdCardModule} from '@angular2-material/card';
+import {MdButtonModule} from '@angular2-material/button';
+import {MdIconModule} from '@angular2-material/icon';
+
+import {MdIconRegistry} from '@angular2-material/icon';
+
+@NgModule({
+  imports:      [ BrowserModule, MdCardModule, MdButtonModule, MdIconModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [MdIconRegistry]
+})
+export class AppModule { }
